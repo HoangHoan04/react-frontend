a669757172198280bc41aab1af42b8d761eb3887
trungnc113 <nguyencongtrung13@gmail.com>
2026-01-31 09:41:14 +0700

    added tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
@@ -8,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
